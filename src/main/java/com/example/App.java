76f55c6eed40c3994d8158e3e1f9176ad7488aa8
Brijesh-ue <brijeshgowda44@gmail.com>
2026-06-18 14:32:44 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )throws Exception
+    {
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get("https://automationexercise.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("(//a[contains(text(),'Add to cart')])[1]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//u[text()='View Cart']")).click();
+        System.out.println( "Logged In" );
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
